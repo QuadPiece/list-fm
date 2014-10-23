24b99fb0bf0d67a563d2ba37cc74d009d694c5b2
TheHowl <github@thehowl.it>
2014-10-23 21:09:52 +0200

    Remove data.php and ignore it.

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//This thing is used to echo the data while working on it. Not an actual part of the project.
-
-$feed_url = "http://ws.audioscrobbler.com/2.0/user/quadpiece/recenttracks";
-
-$scrobbler_xml = file_get_contents($feed_url);
-
-echo $scrobbler_xml;
