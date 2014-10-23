c4f11ea38858dd75094554adcc52ded09baaa78f
TheHowl <github@thehowl.it>
2014-10-23 21:22:08 +0200

    Friendly reminder: TPL > all

--- fetch.php
+++ fetch.php
@@ -2,46 +2,47 @@
 
 //URL for feed (Make sure it's the 2.0 version)
 $feed_url = "http://ws.audioscrobbler.com/2.0/user/quadpiece/recenttracks";
- 
+
+$output = "";
  //Get all the shit
 if ($feed_xml = file_get_contents($feed_url)) {
         $feed_data = simplexml_load_string($feed_xml);        
         
-        //Echo the actual list
-        echo '<div class="trackscontainer">';
-        echo '<ul>';
+        //$output .= the actual list
+        $output .= '<div class="trackscontainer">';
+        $output .= '<ul>';
         foreach ($feed_data->track as $track) {
-                echo '<li class="singletrack">';
+                $output .= '<li class="singletrack">';
 
                 //Check if there is a cover image to use.
                 if ($track->image[3] != "") {
-                        echo '<div class="cover"><img class="cover" src="' . $track->image[3] . '" /></div>';
+                        $output .= '<div class="cover"><img class="cover" src="' . $track->image[3] . '" /></div>';
                 }
 
-                //Echo track artist, title and link
-                echo '<div class="tracktextcontainer">';
-                echo '<p class="tracktext"><span class="title">' . $track->artist . '</span><a href="' . $track->url . '"><br />' . $track->name . '</a></p>';
+                //$output .= track artist, title and link
+                $output .= '<div class="tracktextcontainer">';
+                $output .= '<p class="tracktext"><span class="title">' . $track->artist . '</span><a href="' . $track->url . '"><br />' . $track->name . '</a></p>';
 
-                //Echo N/A as time is song is currently being played
+                //$output .= N/A as time is song is currently being played
                 if (!isset($track['nowplaying'])){
-                        echo '<p class="tracktext">&nbsp;&nbsp;| Played: ' . $track->date . ' <small>(GMT)</small></p>';
+                        $output .= '<p class="tracktext">&nbsp;&nbsp;| Played: ' . $track->date . ' <small>(GMT)</small></p>';
 
                 }
                 else {
-                        echo '<p class="tracktext">&nbsp;&nbsp;| Played: N/A</p>';
+                        $output .= '<p class="tracktext">&nbsp;&nbsp;| Played: N/A</p>';
                 }
 
-                echo '</div>';
+                $output .= '</div>';
 
-                //Echo that fancy Now Playing text thingy
+                //$output .= that fancy Now Playing text thingy
                 if (isset($track['nowplaying']) && $track['nowplaying'] == 'true'){
-                        echo '<p class="nowplaying"><span class="title">Now playing!</p>';
+                        $output .= '<p class="nowplaying"><span class="title">Now playing!</p>';
 
                 }
-                echo '</li>';
+                $output .= '</li>';
         }
-        echo '</ul>';
-        echo '</div>';
+        $output .= '</ul>';
+        $output .= '</div>';
 }
 
 //Copyright Quad - Messy PHP v. 420.69 update codename "blaze" revision 1337
--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
-
-//This will only edit the site and has no effect on the back-end at all
-$username = "Quad"
-
-?>
-
-<!-- list.fm by Leafcat -->
-
-<html>
-	<head>
-		<title>Recent last.fm tracks by <?php echo $username ?></title>
-		<link rel="stylesheet" type="text/css" href="tracklist.css">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<meta charset="UTF-8">
-	</head>
-	<body>
-		<p class="streamtitle">Recently played tracks<small> (via Last.fm)</small></p>
-
-		<?php include_once 'fetch.php' ?>
-
-	</body>
-</html>
+require_once "tpl.php";
+require_once "fetch.php";
+$username = "Quad";
+$tpl = new RainTPL;
+$assign = array(
+	"username" => $username,
+	"content" => $output,
+	);
+$tpl->assign($assign);
+echo $tpl->draw('page', $return_string = true);
