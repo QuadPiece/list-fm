1afc5cae7651ba3f55cc7f613f78d78ab7e6aa35
TheHowl <github@thehowl.it>
2014-10-25 00:30:46 +0200

    IT FUCKING WORKS
    foreach: index.php to page.html: done. Now I can go and sleep.

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once "tpl.php";
 require_once "cfg.php";
+function xml2array ( $xmlObject, $out = array () )
+{
+    foreach ( (array) $xmlObject as $index => $node )
+        $out[$index] = ( is_object ( $node ) ) ? xml2array ( $node ) : $node;
+
+    return $out;
+}
 if (URL_USER) {
 	if (isset($_GET["u"])) {
 		$username = $_GET["u"];
@@ -24,32 +31,12 @@ if (file_exists("cached/" . $username . ".html")) {
 $feed_url = "http://ws.audioscrobbler.com/2.0/user/" . $username . "/recenttracks";
 $output = "";
 if ($feed_xml = file_get_contents($feed_url)) {
-	$feed_data = simplexml_load_string($feed_xml);
-	foreach ($feed_data->track as $track) {
-		$output .= '<li class="singletrack">';
-		if ($track->image[3] != "") {
-			$output .= '<div class="cover"><img class="cover" src="' . $track->image[3] . '" /></div>';
-		}
-		$output .= '<div class="tracktextcontainer"><p class="tracktext"><span class="title">' . $track->artist . '</span><a href="' . $track->url . '"><br />' . $track->name . '</a></p>';
-		if (!isset($track['nowplaying'])){
-			$output .= '<p class="tracktext">&nbsp;&nbsp;| Played: ' . $track->date . ' <small>(GMT)</small></p>';
-
-		}
-		else {
-			$output .= '<p class="tracktext">&nbsp;&nbsp;| Played: Now!</p>';
-		}
-		$output .= '</div>';
-		if (isset($track['nowplaying']) && $track['nowplaying'] == 'true'){
-			$output .= '<p class="nowplaying"><span class="title">Now playing!</p>';
-
-		}
-		$output .= '</li>';
-	}
+	$feed_data = xml2array(simplexml_load_string($feed_xml));
 }
 $tpl = new RainTPL;
 $assign = array(
 	"username" => $username,
-	"content" => $output,
+	"content" => $feed_data["track"],
 	"pagetitle" => TITLE_TAG,
 	"customcss" => CSS_DIR,
 	"usestreamtitle" => USESTREAMTITLE,
