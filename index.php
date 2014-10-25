45ccf0c361f0ca8585b36ca270240091f01f8037
TheHowl <github@thehowl.it>
2014-10-25 12:00:38 +0200

    Sweet errors.

--- index.php
+++ index.php
@@ -8,6 +8,23 @@ function xml2array ( $xmlObject, $out = array () )
 
     return $out;
 }
+function error($error, $username) {
+	$tpl = new RainTPL;
+	$assign = array(
+		"customcss" => CSS_DIR,
+		"usestreamtitle" => USESTREAMTITLE,
+		"streamtitle" => CUST_STREAM_TITLE,
+		"cache" => CACHE,
+		"error" => $error,
+		"username" => $username,
+		);
+	$tpl->assign($assign);
+	$draw = $tpl->draw('error', $return_string = true);
+	if (CACHE) {
+		file_put_contents("cached/" . $username . ".html", $draw);
+	}
+	echo $draw;
+}
 if (URL_USER) {
 	if (isset($_GET["u"])) {
 		$username = $_GET["u"];
@@ -31,8 +48,15 @@ if (CACHE) {
 	}
 }
 $feed_url = "http://ws.audioscrobbler.com/2.0/user/" . $username . "/recenttracks?limit=" . LIMIT;
-if ($feed_xml = file_get_contents($feed_url)) {
+if ($feed_xml = @file_get_contents($feed_url)) {
+	if ($feed_xml == "ERROR: No user with that name was found") {
+		error("no user with that name was found!", $username);
+	}
 	$feed_data = xml2array(simplexml_load_string($feed_xml));
+}
+else {
+	error("php can't call the API url. This can happen because the API is temporary down, or because we can't access the API url, or because that user doesn't exist.", $username);
+	exit;
 }
 $tpl = new RainTPL;
 $assign = array(
